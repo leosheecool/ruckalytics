195d53746daebd0a0403d0692b8ba12038f7556d
Sami Anki <sami.anki1316@gmail.com>
2023-03-15 17:51:20 +0100

    feat: Add Bottom Nav Bar

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header';
 export { default as DetailsHeader } from './DetailsHeader/DetailsHeader';
 export { default as MatchCard } from './MatchCard/MatchCard';
+export { default as BottomNavBar } from './BottomNavBar/BottomNavBar';
