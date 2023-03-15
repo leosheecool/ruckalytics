cd1015ccb035facdd630f0d9efe5cf06e9ed8eeb
Léo Levacher <leo.levacher@sogeti.com>
2023-03-15 14:04:27 +0100

    feat: update type

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,1 +1,3 @@
+export { default as Header } from './Header/Header';
+export { default as DetailsHeader } from './DetailsHeader/DetailsHeader';
 export { default as MatchPreviewCard } from './MatchPreviewCard/MatchPreviewCard';
