94a32feb6d3ff2df42e8a923ad0f963e214abe35
Léo Levacher <leo.levacher@sogeti.com>
2023-03-15 14:15:44 +0100

    feat: add real images

--- src/Pages/Match/Stats/Stats.tsx
+++ src/Pages/Match/Stats/Stats.tsx
@@ -1,32 +1,29 @@
+import { matchs } from 'Mocks/matchs.mocks';
 import React from 'react';
 import styles from './Stats.module.scss';
 
 const Stats = () => {
+	const mockedData = matchs[0];
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
-				<p className={styles.globalInformation}>Location</p>
-				<p className={styles.globalInformation}>date</p>
+				<p className={styles.globalInformation}>{mockedData.location}</p>
+				<p className={styles.globalInformation}>{mockedData.utcDate}</p>
 				<div className={styles.scoreContainer}>
 					<div>
-						<img
-							src="https://api-agregateur-static.ffr.fr/assets/embleme/club/111.jpg"
-							alt="club"
-							className={styles.clubLogo}
-						/>
-						<p>Co Multisport Bagneux Rugby</p>
+						<img src={mockedData.homeTeam.logo} alt="club" className={styles.clubLogo} />
+						<p>{mockedData.homeTeam.name}</p>
 					</div>
 					<div>
-						<p className={styles.score}>3 : 0 </p>
-						<p>40'</p>
+						<p className={styles.score}>
+							{mockedData.score.homeTeam} : {mockedData.score.visitorTeam}{' '}
+						</p>
+						<p>{mockedData.duration}'</p>
 					</div>
 					<div>
-						<img
-							src="https://api-agregateur-static.ffr.fr/assets/embleme/club/1745.jpg"
-							alt="club"
-							className={styles.clubLogo}
-						/>
-						<p>Ass Sportive Fontenaisienne</p>
+						<img src={mockedData.visitorTeam.logo} alt="club" className={styles.clubLogo} />
+						<p>{mockedData.visitorTeam.name}</p>
 					</div>
 				</div>
 			</div>
