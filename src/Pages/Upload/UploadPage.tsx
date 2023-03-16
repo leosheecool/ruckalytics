b3a8ccfe4a49be8123b545de68943888648d8a80
Léo Levacher <leo.levacher@sogeti.com>
2023-03-16 11:36:37 +0100

    refactor (upload page): functions

--- src/Pages/Upload/UploadPage.tsx
+++ src/Pages/Upload/UploadPage.tsx
@@ -5,6 +5,7 @@ import { RiVideoUploadFill } from 'react-icons/ri';
 import { FaFileUpload } from 'react-icons/fa';
 import styles from './UploadPage.module.scss';
 import { useEffect, useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
 
 const UploadPage = () => {
 	const [numPages, setNumPages] = useState<number>();
@@ -18,15 +19,31 @@ const UploadPage = () => {
 
 	function onDocumentLoadSuccess({ numPages: newPagesNb }: { numPages: number }) {
 		setNumPages(newPagesNb);
+		const textLayers = document.querySelectorAll('.react-pdf__Page__textContent textLayer');
+		textLayers.forEach((layer) => {
+			layer.setAttribute('style', 'top: 0; left: 0; transform: none;');
+		});
 	}
 
+	const setBlobVideoFile = (file: Blob) => {
+		setVideoFile(file);
+	};
+
+	const setBlobPdfFile = (file: Blob) => {
+		setFile(file);
+	};
+
 	useEffect(() => {
-		pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-	});
+		// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+		pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
+	}, []);
 
 	// function that creates a blob from file and set it as state
 	// function that creates a blob from file and set it as state
-	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleFileChange = (
+		e: React.ChangeEvent<HTMLInputElement>,
+		changeFn: (file: Blob) => void
+	) => {
 		const newFile = e.target.files?.[0];
 		if (newFile) {
 			const reader = new FileReader();
@@ -36,23 +53,7 @@ const UploadPage = () => {
 					const blob = new Blob([new Uint8Array(buffer as ArrayBuffer)], {
 						type: newFile.type,
 					});
-					setFile(blob);
-				}
-			};
-			reader.readAsArrayBuffer(newFile);
-		}
-	};
-	const handleVideoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const newFile = e.target.files?.[0];
-		if (newFile) {
-			const reader = new FileReader();
-			reader.onload = (e) => {
-				const buffer = e.target?.result;
-				if (buffer) {
-					const blob = new Blob([new Uint8Array(buffer as ArrayBuffer)], {
-						type: newFile.type,
-					});
-					setFile(blob);
+					changeFn(blob);
 				}
 			};
 			reader.readAsArrayBuffer(newFile);
@@ -64,36 +65,61 @@ const UploadPage = () => {
 			<PageHeader title="Upload" />
 			<div className={styles.container}>
 				<div className={globalStyles.card}>
-					<p className={styles.sectionTitle}>Importez la vidéo d'un match ici</p>
-					<button className={styles.button} onClick={() => videoRef.current?.click()}>
-						<RiVideoUploadFill size={48} fill="#fff" />
-					</button>
-					<input
-						type="file"
-						ref={videoRef}
-						style={{ display: 'none' }}
-						onChange={handleVideoChange}
-					/>
+					<div>
+						<p className={styles.sectionTitle}>Importez la vidéo d'un match ici</p>
+						{videoFile && (
+							<ReactPlayer
+								className={styles.pdf}
+								url={videoFile ? URL.createObjectURL(videoFile) : ''}
+								width="auto"
+								height={400}
+								controls={true}
+							/>
+						)}
+						<button className={styles.button} onClick={() => videoRef.current?.click()}>
+							<RiVideoUploadFill size={48} fill="#fff" />
+						</button>
+						<input
+							type="file"
+							ref={videoRef}
+							style={{ display: 'none' }}
+							typeof="video/*"
+							accept="video/*"
+							onChange={(e) => handleFileChange(e, setBlobVideoFile)}
+						/>
+					</div>
 					<div className={styles.separator} />
-					<p className={styles.sectionTitle}>Importez la fiche de match correspondante ici</p>
-					<Document
-						file={file ? file : 'https://arxiv.org/pdf/quant-ph/0410100.pdf'}
-						onLoadSuccess={onDocumentLoadSuccess}
-					>
-						<Page pageNumber={pageNumber} />
-					</Document>
-					<p>
-						Page {pageNumber} of {numPages}
-					</p>
-					<button className={styles.button} onClick={() => fileRef.current?.click()}>
-						<FaFileUpload size={48} fill="#fff" />
-					</button>
-					<input
-						type="file"
-						ref={fileRef}
-						style={{ display: 'none' }}
-						onChange={handleFileChange}
-					/>
+					<div>
+						<p className={styles.sectionTitle}>Importez la fiche de match correspondante ici</p>
+						{file && (
+							<>
+								<Document file={file ? file : ''} onLoadSuccess={onDocumentLoadSuccess}>
+									<Page
+										pageNumber={pageNumber}
+										renderTextLayer={false}
+										renderAnnotationLayer={false}
+										className={styles.pdf}
+										height={500}
+									/>
+								</Document>
+								<p>
+									Page {pageNumber} of {numPages}
+								</p>
+							</>
+						)}
+						<button className={styles.button} onClick={() => fileRef.current?.click()}>
+							<FaFileUpload size={48} fill="#fff" />
+						</button>
+						<input
+							type="file"
+							ref={fileRef}
+							style={{ display: 'none' }}
+							onChange={(e) => handleFileChange(e, setBlobPdfFile)}
+							typeof="application/pdf"
+							accept="application/pdf"
+						/>
+					</div>
+					<button className={styles.button}>Confirmer l'envoi</button>
 				</div>
 			</div>
 		</div>
